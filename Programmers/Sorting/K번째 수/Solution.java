e0d23443cf24126bae7aa842f3bad6ffba963bad
pjy4743 <pjy4743@naver.com>
2020-07-03 23:23:15 +0900

    Update_Programmers_KthNumber

---
+++ Programmers/Sorting/K번째 수/Solution.java
@@ -0,0 +1,14 @@
+/**
+* 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째수를 구하는 프로그램
+*
+* @date 2020. 07. 03
+* @author Jaewon Park
+*/
+
+public class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer
+
+        return answer;
+    }
+}
