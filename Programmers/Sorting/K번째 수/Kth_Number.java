ee1598160a5ee75e7226c2e3d9fd188a349179d9
pjy4743 <pjy4743@naver.com>
2020-07-04 01:40:24 +0900

    Update_Kth_Number

--- Programmers/Sorting/K번째 수/Kth_Number.java
+++ Programmers/Sorting/K번째 수/Kth_Number.java
@@ -5,16 +5,17 @@
 * @author Jaewon Park
 */
 
-public class Solution {
+class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int answerInit = 0;
-        int[] answer = {};
+        int[] answer = new int[50];
 
         for(int i=0; i<commands.length; i++) {
             for(int j=0; j<commands[i].length; j++) {
                 int [] tmp = getSubArray(array, commands[i]);
                 insertSort(tmp, 0, tmp.length);
-                answer[answerInit++] = tmp;
+                answer[answerInit++] = tmp[commands[i][2]];
+                System.out.println("haha");
             }
         }
 
@@ -23,9 +24,9 @@ public class Solution {
 
     public int [] getSubArray(int [] array, int [] subCommands) {
         int init = 0;
-        int [] temp = {};
-        for(int k=subCommands[0]; k<subCommands[1]; k++) {
-            temp[init++] = array[k];
+        int [] temp = new int[50];
+        for(int k=subCommands[0]; k<=subCommands[1]; k++) {
+            temp[init++] = array[k-1];
         }
 
         return temp;
@@ -35,17 +36,19 @@ public class Solution {
         for(int i=1; i<r; i++) {
             int j;
             int subTmp = subArray[i];
-            for(j=i; j>0 && subArray[j-1] > tmp; j--)
+            for(j=i; j>p && subArray[j-1] > subTmp; j--)
                 subArray[j] = subArray[j-1];
             subArray[j] = subTmp;
         }
     }
+}
 
+public class Kth_Number {
     public static void main(String [] args) {
         int [] array = {1, 5, 2, 6, 3, 7, 4};
         int [][] commands = {{2,5,3}, {4,4,1}, {1,7,3}};
 
-        int [] answer = new Solution.solution(array, commands);
+        int [] answer = new Solution().solution(array, commands);
 
         int i = 0;
         while(answer[i] != 0) {
